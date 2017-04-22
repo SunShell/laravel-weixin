6e9fdfbeab6b69de68fcf203c50e9fb4c89de2d9
SunShell <imsunshell@163.com>
2017-04-22 22:32:20 +0800

    回复测试

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -15,8 +15,16 @@ class WechatController extends Controller
     {
 
         $wechat = app('wechat');
+
         $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注铝融网！";
+            switch ($message->MsgType) {
+                case 'text':
+                    return '欢迎关注铝融网！';
+                    break;
+                default:
+                    return '亲，欢迎加入到铝融网免费找货卖货微信服务哦！你有困难我帮助，你有采购我来找。轻松写下你的需求：发语音、发文字我统统照收，并且免费报价报给您哦！不过，您要留下姓名和联系方式，我们会尽快联系您，为您找货！我们的客服热线0533-6202989，欢迎您随时来骚扰！';
+                    break;
+            }
         });
 
         return $wechat->server->serve();
