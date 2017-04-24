0a8eeef032b7945777df6b4ced1987d3979d1a4e
SunShell <imsunshell@163.com>
2017-04-24 23:35:46 +0800

    分享问题

--- resources/views/layouts/vote.blade.php
+++ resources/views/layouts/vote.blade.php
@@ -1,3 +1,6 @@
+<?php
+$js = EasyWeChat::js();
+?>
 <!doctype html>
 <html>
 <head>
@@ -44,5 +47,38 @@
             alert('{{ $res }}');
         </script>
     @endif
+
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" charset="utf-8">
+        wx.config(<?php echo $js->config(array('onMenuShareTimeline','onMenuShareAppMessage'), false) ?>);
+
+        wx.onMenuShareTimeline({
+            title   : '{{ $vote->name }}', // 分享标题
+            link    : 'http://www.lvshangwang.com/vote/1493022390JT13lCdFoh', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl  : 'http://www.lvshangwang.com/storage/topImages/14930223905j0mS.jpg', // 分享图标
+            success : function () {
+                // 用户确认分享后执行的回调函数
+                alert('分享成功！');
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title   : '{{ $vote->name }}', // 分享标题
+            desc    : '',
+            link    : 'http://www.lvshangwang.com/vote/1493022390JT13lCdFoh', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl  : 'http://www.lvshangwang.com/storage/topImages/14930223905j0mS.jpg', // 分享图标
+            type    : 'link', // 分享类型,music、video或link，不填默认为link
+            success : function () {
+                // 用户确认分享后执行的回调函数
+                alert('分享成功！');
+            },
+            cancel  : function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    </script>
 </body>
 </html>
