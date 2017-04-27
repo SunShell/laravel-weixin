4b5fff404be06bfb66c4b447f9dd2b09a29d0567
SunShell <imsunshell@163.com>
2017-04-27 17:05:26 +0800

    自动回复更新

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -54,6 +54,11 @@ class WechatController extends Controller
 
                             $wechat->staff->message($news)->to($message->FromUserName)->send();
                             break;
+                        case '情侣':
+                            $image = new Image(['media_id' => '18gcYy6GNI26QOrkRRtmIsdKFxWWi6hva7ZJTLoyr_Y']);
+
+                            $wechat->staff->message($image)->to($message->FromUserName)->send();
+                            break;
                         case '爬树':
                             $image = new Image(['media_id' => '18gcYy6GNI26QOrkRRtmIgteFogpmBeObiqZvHunmJo']);
 
